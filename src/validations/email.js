98123a90137ede3be5a7094b6acbc0a787704712
Rodrigo Noal <rodrigonoal@gmail.com>
2021-11-11 08:45:28 -0300

    task: added email validation

--- src/validations/email.js
+++ src/validations/email.js
@@ -1,4 +1,4 @@
-function isValidEmail(email) {
+const emailValidation = (email) => {
   if (!email) return false;
 
   if (email.length > 254) return false;
@@ -7,3 +7,5 @@ function isValidEmail(email) {
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return regex.test(String(email).toLowerCase());
 }
+
+module.exports = emailValidation;
